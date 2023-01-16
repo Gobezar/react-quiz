c8c1c825fa547cc676f89d0857e4ac7eb8aa6480
Gobezar <vadimkiselevv@yandex.ru>
2023-01-16 14:23:12 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const questions = [
 function Result({correct}) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <img alt={1} src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из 3</h2>
       <a href='/'>
       <button>Попробовать снова</button>
